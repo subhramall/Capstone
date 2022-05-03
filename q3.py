d78302f9970de4021ae1a72dfb8a9daa70ac13f5
deep <farkadedeep1999@gmail.com>
2022-05-04 05:13:29 +0530

    Make it better

--- q3.py
+++ q3.py
@@ -1,14 +1,104 @@
-from flask import Flask
+# Importing essential libraries and modules
 
-app = Flask(__name__) #constructor of the flask
+from flask import Flask, render_template, request, Markup
+import numpy as np
+import pandas as pd
+import requests
+#import config
+import pickle
+import io
 
-@app.route('/')
-def base_route():
-    return "Welcome to Praxis"
 
-@app.route("/my_name/<name>")
-def print_name(name):
-    return f"Welcome{name}" 
+
+
+from flask import Flask, request
+from flasgger import Swagger
+import pickle
+#import sklearn
+#import scikit_posthocs
+from sklearn.linear_model import LogisticRegression
+from flask import Flask, render_template, request, Markup
+
+
+crop_recommendation_model_path = 'crop1.pkl'
+crop_recommendation_model = pickle.load(
+    open(crop_recommendation_model_path, 'rb'))
+
+
+
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+
+app = Flask(__name__)
+
+# render home page
+
+
+@ app.route('/')
+def home():
+    title = 'Harvestify - Home'
+    return render_template('index.html', title=title)
+
+# render crop recommendation form page
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'Harvestify - Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# render crop recommendation result page
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop1.pkl.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+
+
 
 if __name__ == "__main__":
-    app.run( port = 8000,debug = True)
+    app.run(debug = True)
